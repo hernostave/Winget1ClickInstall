75b5746a50528f3fd7b6c1baa2f177e77c24bde0
Localhost\elbru <herno.stave4@outlook.com>
2023-09-20 14:21:27 +0300

    Final

--- Sas.bat
+++ Sas.bat
@@ -1,5 +1,6 @@
- winget install --id 9NBLGGH4NNS1
+winget install --id 9NBLGGH4NNS1
 winget settings --enable LocalManifestFiles
+curl -L https://raw.githubusercontent.com/hernostave/sas/main/123.reg
 regedit /s 123.reg
 winget source reset --force
 winget source update
