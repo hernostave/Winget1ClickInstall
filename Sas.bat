abc2af3687e6a2988ff13185b74870bdf1acc72e
Localhost\elbru <herno.stave@yandex.ru>
2023-09-22 12:54:22 +0300

    Update Sas.bat

--- Sas.bat
+++ Sas.bat
@@ -1,3 +1,4 @@
+powershell Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 winget install --id 9NBLGGH4NNS1
 winget settings --enable LocalManifestFiles
 curl -OL https://raw.githubusercontent.com/hernostave/sas/main/123.reg
